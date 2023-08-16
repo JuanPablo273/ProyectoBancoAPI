72fd450a42515a3a1962d93f52590985f68a422d
Juan Pablo Martinez <jpms19992015@gmail.com>
2023-08-16 10:34:43 -0600

    Commit API en paralelo con el Web solo por seguimiento

--- ProyectoBancoAPI/Models/UtilitariosModel.cs
+++ ProyectoBancoAPI/Models/UtilitariosModel.cs
@@ -17,6 +17,7 @@ namespace ProyectoBancoAPI.Models
             StringBuilder res = new StringBuilder();
             Random rnd = new Random();
             while (0 < length--)
+
             {
                 res.Append(valid[rnd.Next(valid.Length)]);
             }
